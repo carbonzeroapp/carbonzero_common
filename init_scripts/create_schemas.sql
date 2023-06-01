d9f23f8687c4eec15777a3ff3c8357cff0e2ff7e
Adi Gunawan Lase <adiglase6@gmail.com>
2023-06-01 23:04:00 +0700

    add integration service

---
+++ init_scripts/create_schemas.sql
@@ -0,0 +1,5 @@
+-- Create the data management schema
+CREATE SCHEMA IF NOT EXISTS data_management_schema;
+
+-- Create the user management schema
+CREATE SCHEMA IF NOT EXISTS user_management_schema;
